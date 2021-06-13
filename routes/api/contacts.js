9d98236345f9a4bd74bd267ca15d9fe990863440
Zinaida Razinkova <razinkova.zina@gmail.com>
2021-06-13 16:23:31 +0300

    Обновляет ДЗ

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,92 +1,92 @@
-const express = require('express')
-const router = express.Router()
-const contacts = require('../../model')
+const express = require("express");
+const router = express.Router();
+const contacts = require("../../model");
 const {
   validationAddContact,
   validationUpdateContact,
-} = require('./validationContacts')
+} = require("./validationContacts");
 
-router.get('/', async (req, res, next) => {
-  const data = await contacts.listContacts()
+router.get("/", async (req, res, next) => {
+  const data = await contacts.listContacts();
   try {
     return res.status(200).json({
       data,
-      message: 'data loaded',
-    })
+      message: "data loaded",
+    });
   } catch (err) {
     return res.status(400).json({
-      message: 'data is empty',
-    })
+      message: "data is empty",
+    });
   }
-})
+});
 
-router.get('/:contactId', async (req, res, next) => {
+router.get("/:contactId", async (req, res, next) => {
   try {
-    const data = await contacts.getContactById(req.params.contactId)
+    const data = await contacts.getContactById(req.params.contactId);
     if (data) {
       return res.status(200).json({
         data,
-        message: 'data loaded',
-      })
+        message: "data loaded",
+      });
     } else {
       return res.status(404).json({
-        message: 'Not found',
-      })
+        message: "Not found",
+      });
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.post('/', validationAddContact, async (req, res, next) => {
+router.post("/", validationAddContact, async (req, res, next) => {
   try {
-    const data = await contacts.addContact(req.body)
+    const data = await contacts.addContact(req.body);
     return res.status(201).json({
       data,
-      message: 'contact created',
-    })
+      message: "contact created",
+    });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return res.status(400).json({
-      message: 'missing required name field',
-    })
+      message: "missing required name field",
+    });
   }
-})
+});
 
-router.delete('/:contactId', async (req, res, next) => {
+router.delete("/:contactId", async (req, res, next) => {
   try {
-    const data = await contacts.removeContact(req.params.contactId)
+    const data = await contacts.removeContact(req.params.contactId);
     if (data) {
       return res.status(200).json({
         data,
-        message: 'contact deleted',
-      })
+        message: "contact deleted",
+      });
     } else {
       return res.status(404).json({
-        message: 'Not found',
-      })
+        message: "Not found",
+      });
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.put('/:contactId', validationUpdateContact, async (req, res, next) => {
+router.put("/:contactId", validationUpdateContact, async (req, res, next) => {
   try {
-    const data = await contacts.updateContact(req.params.contactId, req.body)
+    const data = await contacts.updateContact(req.params.contactId, req.body);
     if (data) {
       return res.status(200).json({
         data,
-        message: 'contact update',
-      })
+        message: "contact update",
+      });
     } else {
       return res.status(404).json({
-        message: 'Not found',
-      })
+        message: "Not found",
+      });
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
